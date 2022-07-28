b622130c8e3abae42af6a8006901290b29c6da7b
James Jensen <james.jensen@healthcatalyst.com>
2022-07-28 17:10:58 -0600

    Get started with technologies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'resume-app';
+  profile = {
+    technologyGroups: [
+      {
+        title: ".NET Framework and .NET 6",
+        technologies: [
+          { name: "ASP.NET Core" },
+          { name: "Web API" },
+          { name: "C# 10" },
+        ]
+      }
+    ]
+  }
 }
