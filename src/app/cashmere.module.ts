2f2f27acf42bc1b67dc643a81552e658b5938dfe
James Jensen <james.jensen@healthcatalyst.com>
2022-10-25 11:50:05 -0600

    Remove the TileModule from cashmere
    I'm not using the tile styles anymore.

--- src/app/cashmere.module.ts
+++ src/app/cashmere.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
-import { ButtonModule, TileModule, } from '@healthcatalyst/cashmere';
+import { ButtonModule } from '@healthcatalyst/cashmere';
 
 @NgModule({
-  exports: [TileModule, ButtonModule]
+  exports: [ButtonModule]
 })
 export class CashmereModule { }
